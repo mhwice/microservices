3f63ea77a89bff3d2f748c093422f05efe1435ad
Marshall Wice <mhwice@protonmail.com>
2025-07-03 15:26:12 -0700

    feat: add custom error abstract class

---
+++ auth/src/errors/custom-error.ts
@@ -0,0 +1,17 @@
+export abstract class CustomError extends Error {
+  abstract statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+
+  abstract serializeErrors(): {
+    message: string,
+    field?: string
+  }[]
+}
+
+// class MyClass extends CustomError {
+//   statusCode = 43
+// }
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log("Something went wrong", err);
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
-  res.status(400).send({ message: err.message });
+  res.status(400).send({ errors: [{ message: "Something went wrong" }] });
 }
