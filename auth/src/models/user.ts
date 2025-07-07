070a2d74933c16bb9e6e32b0e55f95ad15468511
Marshall Wice <mhwice@protonmail.com>
2025-07-07 15:13:17 -0700

    feat: integrate mongodb and mongoose

---
+++ auth/src/models/user.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+
+interface UserAttrs {
+  email: string,
+  password: string
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build: (attrs: UserAttrs) => UserDoc
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string,
+  password: string
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+// use 'function' instead of arrow function to have correct this binding
+// to the user document rather than this file
+userSchema.pre("save", async function(done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+
+  // must call done at the end
+  done();
+});
+
+// TS hack - add a build method to model to get type safety
+userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,23 +1,31 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
 router.post("/api/users/signup", [
   body("email").isEmail().withMessage("Email must be valid"),
   body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters")
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
 
   const { email, password } = req.body;
-  console.log("creating a user");
 
-  throw new DatabaseConnectionError();
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new BadRequestError("Email in use");
+  }
 
-  res.send({});
+  // TODO - hash password
+
+  const user = User.build({ email, password });
+  await user.save();
+
+  res.status(201).send(user);
 });
 
 export { router as signupRouter };
