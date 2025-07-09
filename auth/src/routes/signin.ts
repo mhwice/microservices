0da8203c71d945ec4ef20842d57d1e8beab2f4bf
Marshall Wice <mhwice@protonmail.com>
2025-07-09 10:17:42 -0700

    feat: add auth logic to endpoints

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,38 @@
 import express from "express";
+import type { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("signIn");
+router.post("/api/users/signin", [
+  body("email").isEmail().withMessage("Email must be valid"),
+  body("password").trim().notEmpty().withMessage("You must supply a password")
+],
+validateRequest,
+async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) throw new BadRequestError("Invalid credentials");
+
+  const passwordsMatch = await Password.compare(existingUser.password, password);
+  if (!passwordsMatch) throw new BadRequestError("Invalid credentials");
+
+  // generate jwt
+  const userJwt = jwt.sign({
+    id: existingUser.id,
+    email: existingUser.email
+  }, process.env.JWT_KEY!); // we have a check in index.js to ensure this is defined
+
+  // store jwt on session obj
+  req.session = { jwt: userJwt };
+
+  res.status(200).send(existingUser);
 });
 
 export { router as signinRouter };
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  res.send("signOut");
+  req.session = null; // remove the cookie
+  res.send({});
 });
 
 export { router as signoutRouter };
