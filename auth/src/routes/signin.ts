31d8d1397e8ec2663e538c139b8afe6a020ae1b3
Marshall Wice <mhwice@protonmail.com>
2025-07-17 13:05:26 -0700

    feat: updated auth service imports to match npm module

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -3,8 +3,7 @@ import type { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@mwecomm/common";
 import { Password } from "../services/password";
 
 const router = express.Router();
