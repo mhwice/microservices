b01774b6ea694107d818751be4505ea1be1bab67
Marshall Wice <mhwice@protonmail.com>
2025-08-18 11:18:14 -0700

    feat: add job queue and event listener to expiration service

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,3 +1,4 @@
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
@@ -20,6 +21,8 @@ const start = async () => {
 
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
 
   } catch (error) {
     console.error(error);
