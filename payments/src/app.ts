e6faafeeb6ea6569206333169cd12a9b1e75c6ad
Marshall Wice <mhwice@protonmail.com>
2025-08-19 12:07:04 -0700

    feat: add payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { currentUser, errorHandler, NotFoundError } from "@mwecomm/common";
+
+const app = express();
+// we are using NGINX as a proxy - express doesnt like that by default
+app.set("trust proxy", true);
+app.use(json());
+// disable encryption so easy to change service language and implementation
+app.use(cookieSession({
+  signed: false,
+  // in the test environment we must disable secure or tests will fail
+  secure: process.env.NODE_ENV !== "test"
+}));
+
+app.use(currentUser);
+
+app.all("*", () => {
+  throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/index.ts
@@ -0,0 +1,42 @@
+import { app } from "./app";
+import mongoose from "mongoose";
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+
+  if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
+
+  if (!process.env.NATS_CLUSTER_ID) throw new Error("NATS_CLUSTER_ID must be defined");
+  if (!process.env.NATS_CLIENT_ID) throw new Error("NATS_CLIENT_ID must be defined");
+  if (!process.env.NATS_URL) throw new Error("NATS_URL must be defined");
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on 3000");
+  });
+}
+
+// probably don't need to wrap everything in the start fn
+// now that node supports top-level async-await
+start();
